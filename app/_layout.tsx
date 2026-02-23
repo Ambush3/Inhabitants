5a0d39b6f1e9af164f0bf1c57af7f48adcf9b031
aaronbush3 <aaronbush3@gmail.com>
2026-02-22 20:54:43 -0500

    Fix header showing white icons instead of black

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
+import {Stack} from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+      <>
+        <StatusBar style="dark" />
+        <Stack screenOptions={{ headerShown: false }} />
+      </>
+  );
 }
