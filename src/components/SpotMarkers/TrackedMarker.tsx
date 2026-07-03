e5a04605bbfb92d6e9fde5abc3799b6455119992
aaronbush3 <aaronbush3@gmail.com>
2026-07-02 21:47:05 -0400

    disable keep tracking

--- src/components/SpotMarkers/TrackedMarker.tsx
+++ src/components/SpotMarkers/TrackedMarker.tsx
@@ -7,15 +7,11 @@ export const TrackedMarker = forwardRef<any, Props>(({ keepActive, ...props }, r
   const [tracking, setTracking] = useState(true);
 
   useEffect(() => {
-    if (keepActive) {
-      setTracking(true);
-      return;
-    }
     const t = setTimeout(() => setTracking(false), 800);
     return () => clearTimeout(t);
-  }, [keepActive]);
+  }, []);
 
-  return <Marker ref={ref} {...props} tracksViewChanges={keepActive || tracking} />;
+  return <Marker ref={ref} {...props} tracksViewChanges={tracking} />;
 });
 
 TrackedMarker.displayName = 'TrackedMarker';
